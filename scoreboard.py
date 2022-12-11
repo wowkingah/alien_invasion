579755ee5f3121bc7ef059eb7ee5c9ef7053d323
Wowkingah <wowkingah@gmail.com>
2022-12-11 12:11:16 +0800

    显示等级

--- scoreboard.py
+++ scoreboard.py
@@ -18,11 +18,12 @@ class Scoreboard():
         # 准备包含最高得分和当前得分的图像
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """将得分转换为一幅渲染的图像"""
         rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Score: " + "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color,
                                             self.ai_settings.bg_color)
 
@@ -34,7 +35,7 @@ class Scoreboard():
     def prep_high_score(self):
         """将最高得分转换为渲染的图像"""
         high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "High Score: " + "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color,
                                                  self.ai_settings.bg_color)
 
@@ -43,7 +44,18 @@ class Scoreboard():
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    def prep_level(self):
+        """将等级转换为渲染的图像"""
+        self.level_image = self.font.render(str(self.stats.level), True, self.text_color,
+                                            self.ai_settings.bg_color)
+
+        # 将等级放在等分下面
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def show_score(self):
         """在屏幕上显示当前得分和最高得分"""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
